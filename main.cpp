6f916a180c67fe15f4b0a5de0ef9f336d9818ad6
AmrElsayyad <amrelsayyad96@gmail.com>
2023-03-14 16:43:12 +0200

    Subscribed to keyspace and keyevent

--- main.cpp
+++ main.cpp
@@ -5,35 +5,37 @@
 using namespace sw::redis;
 
 int main(int argc, char **argv) {
+    // Set connection options
     ConnectionOptions opts;
     opts.host = "127.0.0.1";
     opts.port = 6379;
     opts.socket_timeout = std::chrono::milliseconds(100);
     
-    // Create an Redis object, which is movable but NOT copyable.
+    // Create an Redis object, which is movable but NOT copyable
     auto redis = Redis(opts);
+
+    // Enable keyspace notifications for all the events except "m" and "n"
+    redis.command("CONFIG", "SET", "notify-keyspace-events", "KEA");
 
     // Create a Subscriber.
     auto sub = redis.subscriber();
 
     // Set callback functions.
     sub.on_message([](std::string const& channel, std::string const& msg) {
-        std::cout << channel << ": " << msg << "\n";
+        std::cout << channel << " -> " << msg << "\n";
     });
 
     sub.on_pmessage([](std::string const& pattern, std::string const& channel, std::string const& msg) {
-        std::cout << "[" << pattern << "] " << channel << ": " << msg << "\n";
+        std::cout << channel << " -> " << msg << "\n";
     });
 
     sub.on_meta([](Subscriber::MsgType type, OptionalString channel, long long num) {
         if (channel) {
             switch (type) {
             case Subscriber::MsgType::SUBSCRIBE:
+            case Subscriber::MsgType::PSUBSCRIBE:
                 std::cout << num << ") Subscribed to " << channel.value() << std::endl;
                 break;
-            case Subscriber::MsgType::PSUBSCRIBE:
-                std::cout << num << ") Subscribed to [" << channel.value() << "]\n";
-                break;   
             case Subscriber::MsgType::UNSUBSCRIBE:
             case Subscriber::MsgType::PUNSUBSCRIBE:
                 std::cout << num << ") Unsubscribed to " << channel.value() << std::endl;
@@ -46,11 +48,18 @@ int main(int argc, char **argv) {
         }
     });
 
-    // Subscribe to channels and patterns.
+    // Subscribe to channels
     sub.subscribe("channel1");
     sub.subscribe({"channel2", "channel3"});
 
+    // Subscribe to pattern
     sub.psubscribe("pattern*");
+
+    // Subscribe to __keyspace@0__:*, which means any event happening on all keys in database 0
+    sub.psubscribe("__keyspace@0__:*");
+
+    // Subscribe to __keyevent@0__:*, which means any key affected by an event happening on database 0
+    sub.psubscribe("__keyevent@0__:*");
 
     // Consume messages in a loop.
     while (true) {
